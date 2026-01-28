ea93ffe203db91d118fe811a641039beb0d40823
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-28 14:30:08 +0530

    roboshop project

---
+++ shell-roboshop2/common_code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log
+SCRIPT_DIR=$PWD
+
+
