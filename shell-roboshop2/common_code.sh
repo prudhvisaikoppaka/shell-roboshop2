b3acbed2ed035c6b29d2e27d94478f709891de90
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-28 16:32:18 +0530

    roboshop project

--- shell-roboshop2/common_code.sh
+++ shell-roboshop2/common_code.sh
@@ -11,4 +11,25 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
+#check the user has root priveleges or not
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+if [ $USERID -ne 0 ]
+then
+   echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+   exit 1 #give other than 0 upto 127
+else
+   echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# Validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+  if [ $1 -eq 0 ]
+   then
+      echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+   else
+      echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+      exit 1
+   fi       
+}
--- shell-roboshop2/mongodb.sh
+++ shell-roboshop2/mongodb.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-#check the user has root priveleges or not
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then
-   echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-   exit 1 #give other than 0 upto 127
-else
-   echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-# Validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-  if [ $1 -eq 0 ]
-   then
-      echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-   else
-      echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-      exit 1
-   fi       
-}
 
 cp mongo.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying MongoDB repo"
